e3e49422c0f445d9cdadcf3b4aed6d380d3e5943
Taha Alami Idrissi <3624559-TahaAlamiIdrissi@users.noreply.gitlab.com>
2021-01-28 17:30:48 +0100

    feat : added static files

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const button = document.getElementById("button")
+
+
+// button.addEventListener("click",() => {
+//     alert("ton email c'est : "+ email.value+" et ton mot de passe c'est : "+password.value+" t'es con en gros ?")
+//     email.value=""
+//     password.value=""
+// });
+
